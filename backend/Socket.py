47bcb497406bea88ca7310d2292ec8155dc6ac98
Potestas06 <Patrick@krispel.com>
2023-05-11 14:12:13 +0200

    fixed instant close bug

--- backend/Socket.py
+++ backend/Socket.py
@@ -3,11 +3,13 @@ import websockets
 
 
 async def handler(websocket, path):
-    data = await websocket.recv()
-    reply = f"Data recieved as:  {data}!"
-    await websocket.send(reply)
-
+    async for message in websocket:
+        print(f"Received message: {message}")
+        reply = f"Server received message: {message}"
+        await websocket.send(reply)
+        print(f"Sent reply: {reply}")
 
 start_server = websockets.serve(handler, "localhost", 8000)
+
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
