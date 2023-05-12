5f14f230f53c6cc8720ce571949429d8d2617709
Potestas06 <Patrick@krispel.com>
2023-05-12 08:39:00 +0200

    fixed NetNode closes instantly bug

--- backend/netNode.py
+++ backend/netNode.py
@@ -20,3 +20,6 @@ async def handler(websocket, path):
         print(f"Sent reply: {reply}")
 
 start_server = websockets.serve(handler, "localhost", 8000)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
