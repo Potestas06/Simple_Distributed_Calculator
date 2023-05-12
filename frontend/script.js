9386a49dc80dfd68ca737a186f21cf532e7ab442
Nonmediabutinformatics <jan.schaefli@bluewin.ch>
2023-05-12 13:19:13 +0200

    Added a delete button

--- frontend/script.js
+++ frontend/script.js
@@ -49,13 +49,16 @@ for (var i = 0; i < keys.length; i++) {
             if (operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
                 input.innerHTML = inputVal.replace(/.$/, btnVal);
             }
-
             decimalAdded = false;
         } else if (btnVal == '.') {
             if (!decimalAdded) {
                 input.innerHTML += btnVal;
                 decimalAdded = true;
             }
+        } else if (btnVal == '␡') {
+            var updatedText = input.innerHTML.slice(0, -1);
+            input.innerHTML = updatedText;
+            decimalAdded = false;
         } else {
             input.innerHTML += btnVal;
         }
