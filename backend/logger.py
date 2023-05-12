48605b93bf2c6242ea9cd5a8c8f03112abe9f58f
Nonmediabutinformatics <jan.schaefli@bluewin.ch>
2023-05-12 08:33:49 +0200

    addad a second log to the logger

--- backend/logger.py
+++ backend/logger.py
@@ -5,16 +5,18 @@ import datetime
 
 LOGFILE_PATH = "logfile.log"
 
-async def log(message):
+async def log(message, reply):
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_entry = f"[{current_time}] {message}"
+    log_entry_message = f"[{current_time}] {message}"
+    log_entry_reply = f"[{current_time}] {reply}"
     with open(LOGFILE_PATH, "a") as logfile:
-        logfile.write(f"{log_entry}\n")
+        logfile.write(f"{log_entry_message}\n")
+        logfile.write(f"{log_entry_reply}\n")
 
 async def handler(websocket, path):
     async for message in websocket:
         print(f"Received message: {message}")
-        await log(message)
+        await log(message, reply)
         reply = json.dumps({"status": "success"})
         await websocket.send(reply)
         print(f"Sent reply: {reply}")
