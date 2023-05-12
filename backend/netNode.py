ffe38975e4a153ebd82b606212b87c982259ff35
Potestas06 <Patrick@krispel.com>
2023-05-12 08:34:22 +0200

    added NetNode and made changes to start.bat

--- backend/netNode.py
+++ backend/netNode.py
@@ -1,2 +1,22 @@
 import asyncio
 import websockets
+
+
+async def redirect(message):
+    async with websockets.connect(f"ws://localhost:8001") as websocket:
+        await websocket.send(message)
+        print(f"Sent: {message}")
+
+        response = await websocket.recv()
+        print(f"Result: {response}")
+        return response
+
+
+async def handler(websocket, path):
+    async for message in websocket:
+        print(f"Received message: {message}")
+        reply = await redirect(message)
+        await websocket.send(reply)
+        print(f"Sent reply: {reply}")
+
+start_server = websockets.serve(handler, "localhost", 8000)
