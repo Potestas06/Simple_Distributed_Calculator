3bb4ea504813bee3b94c4e716bcad7ec0bb729f0
Potestas06 <Patrick@krispel.com>
2023-05-12 14:01:50 +0200

    added conection error handling

--- frontend/script.js
+++ frontend/script.js
@@ -1,81 +1,85 @@
 const socket = new WebSocket("ws://localhost:8000");
 
-var keys = document.querySelectorAll('#calc span');
-var operators = ['+', '-', 'x', '÷'];
+var keys = document.querySelectorAll("#calc span");
+var operators = ["+", "-", "x", "÷"];
 var decimalAdded = false;
 
 for (var i = 0; i < keys.length; i++) {
-    keys[i].onclick = function(e) {
-        var input = document.querySelector('.display');
-        var inputVal = input.innerHTML;
-        var btnVal = this.innerHTML;
+  keys[i].onclick = function (e) {
+    var input = document.querySelector(".display");
+    var inputVal = input.innerHTML;
+    var btnVal = this.innerHTML;
 
-        if (btnVal == 'C') {
-            input.innerHTML = '';
-            decimalAdded = false;
-        } else if (btnVal == '=') {
-            var equation = inputVal;
-            var lastChar = equation[equation.length - 1];
+    if (btnVal == "C") {
+      input.innerHTML = "";
+      decimalAdded = false;
+    } else if (btnVal == "=") {
+      var equation = inputVal;
+      var lastChar = equation[equation.length - 1];
 
-            equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+      equation = equation.replace(/x/g, "*").replace(/÷/g, "/");
 
-            if (operators.indexOf(lastChar) > -1 || lastChar == '.'){
-                equation = equation.replace(/.$/, '');
-            };
+      if (operators.indexOf(lastChar) > -1 || lastChar == ".") {
+        equation = equation.replace(/.$/, "");
+      }
 
-            if (equation) {
-                //input.innerHTML = eval(equation);
-                const message = {
-                    work: equation,
-                }
-                socket.send(JSON.stringify(message));
+      if (equation) {
+        //input.innerHTML = eval(equation);
+        const message = {
+          work: equation,
+        };
+        socket.send(JSON.stringify(message));
 
-                socket.onmessage = (event) => {
-                    input.innerHTML = event.data;
-                  };
-                }
+        socket.onmessage = (event) => {
+          input.innerHTML = event.data;
+        };
+      }
 
-            decimalAdded = false;
-        } else if (operators.indexOf(btnVal) > -1) {
-            var lastChar = inputVal[inputVal.length - 1];
+      decimalAdded = false;
+    } else if (operators.indexOf(btnVal) > -1) {
+      var lastChar = inputVal[inputVal.length - 1];
 
-            if (inputVal != '' && operators.indexOf(lastChar) == -1)
-                input.innerHTML += btnVal;
+      if (inputVal != "" && operators.indexOf(lastChar) == -1)
+        input.innerHTML += btnVal;
+      else if (inputVal == "" && btnVal == "-") input.innerHTML += btnVal;
 
-            else if (inputVal == '' && btnVal == '-')
-                input.innerHTML += btnVal;
-
-
-            if (operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
-                input.innerHTML = inputVal.replace(/.$/, btnVal);
-            }
-            decimalAdded = false;
-        } else if (btnVal == '.') {
-            if (!decimalAdded) {
-                input.innerHTML += btnVal;
-                decimalAdded = true;
-            }
-        } else if (btnVal == '␡') {
-            var updatedText = input.innerHTML.slice(0, -1);
-            input.innerHTML = updatedText;
-            decimalAdded = false;
-        } else {
-            input.innerHTML += btnVal;
-        }
-
-        // prevent page jumps
-        e.preventDefault();
+      if (operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
+        input.innerHTML = inputVal.replace(/.$/, btnVal);
+      }
+      decimalAdded = false;
+    } else if (btnVal == ".") {
+      if (!decimalAdded) {
+        input.innerHTML += btnVal;
+        decimalAdded = true;
+      }
+    } else if (btnVal == "␡") {
+      var updatedText = input.innerHTML.slice(0, -1);
+      input.innerHTML = updatedText;
+      decimalAdded = false;
+    } else {
+      input.innerHTML += btnVal;
     }
+
+    // prevent page jumps
+    e.preventDefault();
+  };
 }
 
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
 
 function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-    }
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
 }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+toggleSwitch.addEventListener("change", switchTheme, false);
+
+socket.onclose = function (event) {
+  var input = document.querySelector(".display");
+  input.innerHTML = " lost connection to server";
+};
