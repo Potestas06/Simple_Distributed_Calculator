6799d542ff6a797a18a42bc922a854e316b54938
Samuel Muggli <samuel@meatball.ch>
2023-05-17 10:24:58 +0200

    🧑‍💻New tab replaced new window in start.bat + new folders/files were added to gitignore.

--- start.bat
+++ start.bat
@@ -1,7 +1,10 @@
 cd backend
-start cmd /k python netNode.py
-start cmd /k python spooler.py
-start cmd /k python logger.py
+set "current_path=%CD%"
+wt -w 0 nt -d "%current_path%" -p "Command Prompt" cmd /k python netNode.py
+wt -w 0 nt -d "%current_path%" -p "Command Prompt" cmd /k python spooler.py
+wt -w 0 nt -d "%current_path%" -p "Command Prompt" cmd /k python logger.py
 
-cd calc
-for /L %%i in (1,1,5) do start cmd /k python calc%%i.py
+set "current_path=%CD%\calc"
+for /L %%i in (1,1,5) do (
+  wt -w 0 nt -d "%current_path%" -p "Command Prompt" cmd /k python calc%%i.py
+)
