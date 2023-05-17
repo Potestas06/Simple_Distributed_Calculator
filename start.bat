379f610e9ba276d6597e633888a6cfbff7a1f444
Samuel Muggli <samuel@meatball.ch>
2023-05-17 10:39:14 +0200

    🧑‍💻changed Tab names to current running program in name.

--- start.bat
+++ start.bat
@@ -1,10 +1,10 @@
 cd backend
 set "current_path=%CD%"
-wt -w 0 nt -d "%current_path%" -p "Command Prompt" cmd /k python netNode.py
-wt -w 0 nt -d "%current_path%" -p "Command Prompt" cmd /k python spooler.py
-wt -w 0 nt -d "%current_path%" -p "Command Prompt" cmd /k python logger.py
+wt -w 0 nt -d "%current_path%" -p "Command Prompt" --title netMode cmd /k python netNode.py
+wt -w 0 nt -d "%current_path%" -p "Command Prompt" --title spooler cmd /k python spooler.py
+wt -w 0 nt -d "%current_path%" -p "Command Prompt" --title logger cmd /k python logger.py
 
 set "current_path=%CD%\calc"
 for /L %%i in (1,1,5) do (
-  wt -w 0 nt -d "%current_path%" -p "Command Prompt" cmd /k python calc%%i.py
+  wt -w 0 nt -d "%current_path%" -p "Command Prompt" --title calc%%i cmd /k python calc%%i.py
 )
